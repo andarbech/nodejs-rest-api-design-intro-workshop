dd5442f50bced36a2d0e0ba363b9596ed2f03e02
andarbech <andarbech1.com>
2021-12-17 14:12:05 +0100

    clear

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-const UserModel = require("./user-model");
-const SongModel = require("./song-model");
-const BookModel = require("./book-model");
-
-module.exports = {
-  User: UserModel,
-  Song: SongModel,
-  Book: BookModel,
-};
